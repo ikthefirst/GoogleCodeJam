9f7e0d20479a80754780944d000b0e4b94993bf6
ikthefirst <ikthefirst@gmail.com>
2014-10-03 16:50:48 +0200

    Implemented and tested input parser.

---
+++ Magica/src/main/java/code/google/Parser.java
@@ -0,0 +1,32 @@
+package code.google;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import code.google.data.InputData;
+import code.google.skeleton.AbstractParser;
+
+public class Parser extends AbstractParser<InputData> {
+
+	@Override
+	public InputData parseLine(String line) {
+		InputData inputData = new InputData();
+		String[] tokens = line.split("\\s+");
+		int i = 0;
+		int combineNum = Integer.parseInt(tokens[i]);
+		i++;
+		for (; combineNum > 0; combineNum--, i++) {
+			inputData.addCombineRule(tokens[i]);
+		}
+		int opposeNum = Integer.parseInt(tokens[i]);
+		i++;
+		for (; opposeNum > 0; opposeNum--, i++) {
+			inputData.addOpposeRule(tokens[i]);
+		}
+		int inputLength = Integer.parseInt(tokens[i]);
+		i++;
+		inputData.setInput(tokens[i]);
+
+		return inputData;
+	}
+}
