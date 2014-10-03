4ed5c6e4096d12868d5fd0761ef05dde6212aec0
ikthefirst <ikthefirst@gmail.com>
2014-10-03 16:25:37 +0200

    Added unit tests for multiple rules in master rule.

--- Magica/src/test/java/code/google/logic/TestMasterRule.java
+++ Magica/src/test/java/code/google/logic/TestMasterRule.java
@@ -60,4 +60,60 @@ public class TestMasterRule {
 				output, result);
 	}
 
+	@Test
+	public void twoCombineRulesShouldCombine() {
+		CombineRule combineRule0 = new CombineRule();
+		combineRule0.addRuleElement('X');
+		combineRule0.addRuleElement('Y');
+		combineRule0.setCombinedElement('T');
+
+		CombineRule combineRule1 = new CombineRule();
+		combineRule1.addRuleElement('X');
+		combineRule1.addRuleElement('T');
+		combineRule1.setCombinedElement('Z');
+
+		MasterRule rule = new MasterRule();
+		rule.addRule(combineRule0);
+		rule.addRule(combineRule1);
+
+		String input = "XXY";
+		String output = "Z";
+
+		String result = rule.execute(new StringBuffer(input)).toString();
+
+		assertEquals("Rule should combine the two combine rules.", output,
+				result);
+	}
+
+	@Test
+	public void twoCombineRulesShouldAlterOpposeRule() {
+		CombineRule combineRule0 = new CombineRule();
+		combineRule0.addRuleElement('X');
+		combineRule0.addRuleElement('Y');
+		combineRule0.setCombinedElement('T');
+
+		CombineRule combineRule1 = new CombineRule();
+		combineRule1.addRuleElement('X');
+		combineRule1.addRuleElement('T');
+		combineRule1.setCombinedElement('Z');
+
+		OpposeRule opposeRule = new OpposeRule();
+		opposeRule.addRuleElement('A');
+		opposeRule.addRuleElement('Z');
+
+		MasterRule rule = new MasterRule();
+		rule.addRule(combineRule0);
+		rule.addRule(combineRule1);
+		rule.addRule(opposeRule);
+
+		String input = "AXXXXXXXXY";
+		String output = "";
+
+		String result = rule.execute(new StringBuffer(input)).toString();
+
+		assertEquals(
+				"Rule two combine rules should alter the outcome of oppose rule.",
+				output, result);
+	}
+
 }
