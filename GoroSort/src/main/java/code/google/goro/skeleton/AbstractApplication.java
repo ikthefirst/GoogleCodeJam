2cd8ae48e8db4b19f30f6b78a8f90071f21f67a8
ikthefirst <ikthefirst@gmail.com>
2014-10-06 14:31:50 +0200

    Create initial skeleton project for GoroSort.

---
+++ GoroSort/src/main/java/code/google/goro/skeleton/AbstractApplication.java
@@ -0,0 +1,36 @@
+package code.google.goro.skeleton;
+
+import java.io.IOException;
+import java.util.List;
+
+import code.google.goro.skeleton.AbstractParser;
+
+/**
+ * Abstract application class for executing solutions.
+ */
+public abstract class AbstractApplication<D> {
+
+	public void execute(String[] args) {
+		AbstractParser<D> parser = createParser();
+
+		if (args.length > 0) {
+			try {
+				parser.parseFile(args[0]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				parser.parseFile("in/example.in");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		this.doStuff(parser.getInputData());
+	}
+
+	public abstract AbstractParser<D> createParser();
+
+	public abstract void doStuff(List<D> inputData);
+}
