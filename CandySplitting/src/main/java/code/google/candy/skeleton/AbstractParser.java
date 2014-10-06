a2fd909166c6b6bba68891b9997f8e3d16be85db
ikthefirst <ikthefirst@gmail.com>
2014-10-06 13:35:45 +0200

    Removed outdate AbstractApplication.

---
+++ CandySplitting/src/main/java/code/google/candy/skeleton/AbstractParser.java
@@ -0,0 +1,44 @@
+package code.google.candy.skeleton;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Interface for parsing input.
+ */
+public abstract class AbstractParser<D> {
+
+	private List<D> inputData = new ArrayList<D>();
+
+	public void parseFile(String inputFile) throws IOException {
+
+		File file = new File(inputFile);
+
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line = br.readLine();
+			if (line == null) {
+				return;
+			}
+			int testcaseNum = Integer.parseInt(line);
+			while (testcaseNum-- > 0) {
+				line = br.readLine();
+				D data = parseLine(line);
+				inputData.add(data);
+			}
+		}
+	}
+
+	/**
+	 * Parse line, collect input to input data.
+	 */
+	public abstract D parseLine(String line);
+
+	public List<D> getInputData() {
+		return inputData;
+	}
+
+}
