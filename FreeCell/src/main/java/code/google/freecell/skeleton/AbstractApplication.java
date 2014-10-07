74c73c36f50bd8de0cc61cd51bcb8af526e467ce
ikthefirst <ikthefirst@gmail.com>
2014-10-07 16:44:21 +0200

    Initial project created for FreeCell.

---
+++ FreeCell/src/main/java/code/google/freecell/skeleton/AbstractApplication.java
@@ -0,0 +1,34 @@
+package code.google.freecell.skeleton;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Abstract application class for executing solutions.
+ */
+public abstract class AbstractApplication<D> {
+
+	public void execute(String[] args) {
+		AbstractParser<D> parser = createParser();
+
+		if (args.length > 0) {
+			try {
+				parser.parseFile(args[0]);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+			try {
+				parser.parseFile("in/example.in");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		this.doStuff(parser.getInputData());
+	}
+
+	public abstract AbstractParser<D> createParser();
+
+	public abstract void doStuff(List<D> inputData);
+}
