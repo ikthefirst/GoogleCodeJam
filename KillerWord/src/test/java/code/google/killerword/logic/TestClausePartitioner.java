53ae45e614c3aa3357538ecc1c4bbeda1e8cb9af
ikthefirst <ikthefirst@gmail.com>
2014-10-09 13:54:16 +0200

    Added test for ClausePartitioner.createNewClause()

---
+++ KillerWord/src/test/java/code/google/killerword/logic/TestClausePartitioner.java
@@ -0,0 +1,82 @@
+package code.google.killerword.logic;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import code.google.killerword.data.Clause;
+import static org.junit.Assert.*;
+
+public class TestClausePartitioner {
+
+	public static ClausePartitioner partitioner;
+
+	@BeforeClass
+	public static void createClausePartitioner() {
+		partitioner = new ClausePartitioner();
+	}
+
+	@Test
+	public void buildPatternShouldNotChangePatternOnEmptyPositionsSet() {
+		String pattern = "...xy";
+		Set<Integer> positions = new HashSet<Integer>(
+				Arrays.asList(new Integer[] {}));
+		assertEquals(
+				"ClausePartitioner.buildPattern() should not change pattern on empty positions set.",
+				"...xy", partitioner.buildPattern(pattern, positions, 'a'));
+	}
+
+	@Test
+	public void buildPatternShouldReplaceDotsAtSpecificPositions() {
+		String pattern = "...xy";
+		Set<Integer> positions = new HashSet<Integer>(
+				Arrays.asList(new Integer[] { 0, 1, 2 }));
+		assertEquals(
+				"ClausePartitioner.buildPattern() should replace dots at specific positions.",
+				"aaaxy", partitioner.buildPattern(pattern, positions, 'a'));
+	}
+
+	@Test
+	public void buildPatternShouldNotReplaceCharactersOtherThanDots() {
+		String pattern = "...xy";
+		Set<Integer> positions = new HashSet<Integer>(
+				Arrays.asList(new Integer[] { 2, 3, 4 }));
+		assertEquals(
+				"ClausePartitioner.buildPattern() should replace dots at specific positions.",
+				"..axy", partitioner.buildPattern(pattern, positions, 'a'));
+	}
+
+	@Test
+	public void createNewClauseShouldIncrementLostPointsOnEmptyPositions() {
+		String pattern = "a..";
+		Set<String> words = new HashSet<>(Arrays.asList(new String[] { "abb",
+				"abc" }));
+		Set<Integer> positions = new HashSet<>(Arrays.asList(new Integer[] {}));
+		Set<Character> chars = new HashSet<>(Arrays.asList(new Character[] {
+				'a', 'b' }));
+		Clause newClause = partitioner.createNewClause(pattern, positions,
+				words, chars, 0);
+		assertEquals(
+				"ClausePartitioner.createNewClause() should increment lost points on empty positions.",
+				1, newClause.getLostPoints());
+	}
+
+	@Test
+	public void createNewClauseShouldNotIncrementLostPointsOnNonEmptyPositions() {
+		String pattern = "a..";
+		Set<String> words = new HashSet<>(Arrays.asList(new String[] { "abb",
+				"abc" }));
+		Set<Integer> positions = new HashSet<>(
+				Arrays.asList(new Integer[] { 2 }));
+		Set<Character> chars = new HashSet<>(Arrays.asList(new Character[] {
+				'a', 'b' }));
+		Clause newClause = partitioner.createNewClause(pattern, positions,
+				words, chars, 0);
+		assertEquals(
+				"ClausePartitioner.createNewClause() should increment lost points on empty positions.",
+				0, newClause.getLostPoints());
+	}
+}
