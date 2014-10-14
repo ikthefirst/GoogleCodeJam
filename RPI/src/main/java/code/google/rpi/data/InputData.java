e0a76d3a45575117ce0226ef4d87aa3513b9c572
ikthefirst <ikthefirst@gmail.com>
2014-10-14 14:40:33 +0200

    InputData created.

--- RPI/src/main/java/code/google/rpi/data/InputData.java
+++ RPI/src/main/java/code/google/rpi/data/InputData.java
@@ -2,4 +2,34 @@ package code.google.rpi.data;
 
 public class InputData {
 
+	// win : 1
+	// lose : 0
+	// not played: -1
+	private byte[][] games;
+
+	// number of teams
+	private int N;
+
+	public InputData(int n) {
+		super();
+		this.N = n;
+		this.games = new byte[N][N];
+	}
+
+	public byte[][] getGames() {
+		return games;
+	}
+
+	public void setGames(byte[][] games) {
+		this.games = games;
+	}
+
+	public int getN() {
+		return N;
+	}
+
+	public void setN(int n) {
+		N = n;
+	}
+
 }
